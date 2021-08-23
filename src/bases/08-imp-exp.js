398c38a89edb95c01ba768a9c5b9cdda11145da3
Cristian Suarez <cdsp10@hotmail.com>
2021-08-23 15:01:55 -0500

    Promesas en JS

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,8 +1,8 @@
-import heroes, {owners} from './data/heroes';
+import heroes, {owners} from '../data/heroes';
 
-console.log(owners);
+// console.log(owners);
 
-console.log(heroes);
+// console.log(heroes);
 
 // const getHeroeById = (id) => {
 //     return heroes.find((heroe)=> {
@@ -16,8 +16,11 @@ const getHeroeById = (id) => {
     return heroes.find((heroe)=>heroe.id === id);
 }
 
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
 const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner)
 
-console.log(getHeroesByOwner('DC'));
+// console.log(getHeroesByOwner('DC'));
+
+
+export default getHeroeById;
