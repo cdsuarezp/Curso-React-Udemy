628fb427e2d3b8e37c49446413b9e1a5d5b9d8e4
Cristian Suarez <cdsp10@hotmail.com>
2021-08-23 13:34:41 -0500

    importaciones y exportaciones en JS

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,23 @@
+import heroes, {owners} from './data/heroes';
+
+console.log(owners);
+
+console.log(heroes);
+
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe)=> {
+//         if (heroe.id === id) {
+//             return heroe
+//         }
+//     })
+// };
+
+const getHeroeById = (id) => {
+    return heroes.find((heroe)=>heroe.id === id);
+}
+
+console.log(getHeroeById(2));
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner)
+
+console.log(getHeroesByOwner('DC'));
