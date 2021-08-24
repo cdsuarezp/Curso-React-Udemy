29326fefe81634d9d6f28537d03a23b6dcaf30a5
Cristian Suarez <cdsp10@hotmail.com>
2021-08-24 12:34:11 -0500

    operado terniario en JS

---
+++ src/bases/12-operador-terniario.js
@@ -0,0 +1,18 @@
+
+
+const activo = true;
+
+
+// let mensaje = '';
+
+
+// if (activo) {
+//     mensaje = 'activo';
+// }else{
+//     mensaje = 'inactivo';
+// }
+
+const mensaje = !activo  && 'activo';
+
+
+console.log(mensaje);
