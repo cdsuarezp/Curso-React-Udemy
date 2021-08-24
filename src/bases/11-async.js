823b00ef11d1d677a727401eaf50fab1fd798817
Cristian Suarez <cdsp10@hotmail.com>
2021-08-24 12:27:04 -0500

    asyn y await en js

---
+++ src/bases/11-async.js
@@ -0,0 +1,28 @@
+
+
+const getImagen = async() => {
+
+
+    try{
+        const apiKey='beGMdoKAxvn6sEt53iukkvHcWVyHTLBj';
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const {data} = await resp.json();
+        const {url} = data.images.original;
+    
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+        console.log(data);
+    } catch (error){
+
+    }
+
+   
+}
+
+
+getImagen();
+
+
+
+
